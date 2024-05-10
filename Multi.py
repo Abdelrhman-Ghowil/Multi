77d701a165b2587b3f484e2c1a8671c45123580c
Abdelrhman-Ghowil <104584928+Abdelrhman-Ghowil@users.noreply.github.com>
2024-05-10 19:05:39 +0300

    Add files via upload

---
+++ Multi.py
@@ -0,0 +1,19 @@
+
+# Multi images With Slide bar
+import streamlit as st
+from rembg import remove
+from PIL import Image
+import numpy as np
+
+st.title("rembg Demo")
+col1, col2 = st.columns(2)
+images = st.sidebar.file_uploader("Load Image", "My image", accept_multiple_files =True)
+if images:
+    for image in images:
+        with Image.open(image) as img:
+            col1.header("Original")
+            col1.image(img)
+
+            output = remove(img)
+            col2.header("Extracted")
+            col2.image(output)
