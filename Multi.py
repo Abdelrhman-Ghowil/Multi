b6aea8d9127d8076add8a669bf86954567543ace
Abdelrhman-Ghowil <104584928+Abdelrhman-Ghowil@users.noreply.github.com>
2024-05-10 19:34:49 +0300

    Multi.py

--- Multi.py
+++ Multi.py
@@ -7,7 +7,7 @@ import numpy as np
 
 st.title("rembg Demo")
 col1, col2 = st.columns(2)
-images = st.sidebar.file_uploader("Load Image", "My image", accept_multiple_files =True)
+images = st.sidebar.file_uploader("Load Image", accept_multiple_files =True)
 if images:
     for image in images:
         with Image.open(image) as img:
